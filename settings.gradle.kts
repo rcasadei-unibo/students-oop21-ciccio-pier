54e79766b5d096bff5bad432f259cef4fb4a3e24
Davide Valdifiori <davide.valdifiori@studio.unibo.it>
2022-03-07 18:27:13 +0100

    Initial commit.

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "OOP21-ciccio-pier"
---
+++ src/main/java/it/unibo/cicciopier/App.java
@@ -0,0 +1,16 @@
+package it.unibo.cicciopier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    private App() {
+        // the constructor will never be called directly.
+    }
+
+    public static void main(final String[] args) {
+        
+    }
+}
