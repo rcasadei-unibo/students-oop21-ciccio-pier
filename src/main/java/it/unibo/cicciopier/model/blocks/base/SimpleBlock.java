ceec507de908ba00b33da5987150bdb583d84ed9
Davide Valdifiori <davide.valdifiori@studio.unibo.it>
2022-03-12 11:07:58 +0100

    Create SimpleBlock, SimpleGameObject.

---
+++ src/main/java/it/unibo/cicciopier/model/blocks/base/SimpleBlock.java
@@ -0,0 +1,35 @@
+package it.unibo.cicciopier.model.blocks.base;
+
+import it.unibo.cicciopier.model.SimpleGameObject;
+
+/**
+ * Simple implementation of the interface {@link Block}.
+ */
+public class SimpleBlock extends SimpleGameObject implements Block {
+    private BlockType type;
+
+    /**
+     * Constructor for this class, it instantiates a block with the specific {@link BlockType}.
+     */
+    public SimpleBlock(final BlockType type) {
+        super();
+        this.type = type;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public BlockType getType() {
+        return type;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setType(final BlockType type) {
+        this.type = type;
+    }
+
+}
