46728fedac54e01a41514681a7a22d13aae17698
Mingozzi Riccardo <riccardo.mingozzi4@studio.unibo.it>
2022-03-13 13:49:11 +0100

    Add SimpleMovingEntity class

---
+++ src/main/java/it/unibo/cicciopier/model/entities/base/SimpleMovingEntity.java
@@ -0,0 +1,44 @@
+package it.unibo.cicciopier.model.entities.base;
+
+import it.unibo.cicciopier.model.GameObject;
+import it.unibo.cicciopier.model.World;
+import it.unibo.cicciopier.utility.Vector2d;
+
+public abstract class SimpleMovingEntity extends SimpleEntity implements MovingEntity{
+
+    private Vector2d vel;
+
+    /**
+     * Constructor for this class
+     * @param type The Entity's type
+     * @param world The game's world
+     */
+    protected SimpleMovingEntity(EntityType type, World world) {
+        super(type, world);
+        this.vel = new Vector2d(0,0);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Vector2d getVel() {
+        return this.vel;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setVel(Vector2d vel) {
+        this.vel = vel;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean checkCollision(GameObject object) {
+        return this.getBounds().intersects(object.getBounds());
+    }
+}
