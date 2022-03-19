c4a2312b768553bdac82836f544805372bf6ee0e
Mingozzi Riccardo <riccardo.mingozzi4@studio.unibo.it>
2022-03-19 22:59:07 +0100

    Fix PlayerImpl javadoc

--- src/main/java/it/unibo/cicciopier/model/entities/PlayerImpl.java
+++ src/main/java/it/unibo/cicciopier/model/entities/PlayerImpl.java
@@ -3,8 +3,6 @@ package it.unibo.cicciopier.model.entities;
 import it.unibo.cicciopier.model.World;
 import it.unibo.cicciopier.model.entities.base.EntityType;
 import it.unibo.cicciopier.model.entities.base.SimpleLivingEntity;
-import it.unibo.cicciopier.model.entities.enemies.boss.Missile;
-import it.unibo.cicciopier.utility.Vector2d;
 import it.unibo.cicciopier.view.GameObjectView;
 
 public class PlayerImpl extends SimpleLivingEntity implements Player {
@@ -14,22 +12,37 @@ public class PlayerImpl extends SimpleLivingEntity implements Player {
     private final int attackDamage;
     private int stamina;
 
+    /**
+     * Constructor for this class
+     *
+     * @param world The game's world
+     * @param type The entity's type
+     */
     public PlayerImpl(final EntityType type, final World world) {
         super(type, world);
         this.stamina = maxStamina;
         this.attackDamage = this.getType().getAttackDamage();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getStamina() {
         return this.stamina;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getMaxStamina() {
         return this.maxStamina;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void addStamina(final int amount) {
         this.stamina += amount;
@@ -38,6 +51,9 @@ public class PlayerImpl extends SimpleLivingEntity implements Player {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void decreaseStamina(final int amount) {
         this.stamina -= amount;
@@ -46,16 +62,25 @@ public class PlayerImpl extends SimpleLivingEntity implements Player {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void attackNearest() {
         //TODO
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void tick() {
         //TODO
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void jump() {
         //TODO
