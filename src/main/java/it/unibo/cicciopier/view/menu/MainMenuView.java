f5cc2280314bdc251d9b0b7f03962b067473e824
GJKnOcKOuT <alessandro.aldini00@outlook.it>
2022-03-22 12:42:58 +0100

    Add menu test image, settings and button in MainMenuView

--- src/main/java/it/unibo/cicciopier/view/menu/MainMenuView.java
+++ src/main/java/it/unibo/cicciopier/view/menu/MainMenuView.java
@@ -1,10 +1,27 @@
 package it.unibo.cicciopier.view.menu;
 
+import it.unibo.cicciopier.App;
 import it.unibo.cicciopier.controller.Engine;
+import it.unibo.cicciopier.model.entities.Player;
 import it.unibo.cicciopier.view.View;
 
-public class MainMenuView implements View {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+
+public class MainMenuView extends JPanel implements View {
     private final Engine engine;
+    private final String path = "/menuGraphics/menuBackground.png";
+    private BufferedImage background;
+    private final JFrame jframe = new JFrame("CICCIO PIER THE GAME");
+    private final JButton play = new JButton("Play");
+    private final JButton leaderboard = new JButton("Leaderboard");
+    private final JButton settings = new JButton("Settings");
+    private final JButton quit = new JButton("Quit");
+    Dimension size = new Dimension(1536,768);
+
 
     public MainMenuView(Engine engine) {
         this.engine = engine;
@@ -15,6 +32,18 @@ public class MainMenuView implements View {
      */
     @Override
     public void load() throws Exception {
+        final InputStream is = getClass().getResourceAsStream(path);
+        background = ImageIO.read(is);
+        this.add(play);
+        this.add(leaderboard);
+        this.add(settings);
+        this.add(quit);
+        jframe.getContentPane().add(this);
+        jframe.pack();
+        jframe.setResizable(false);
+        jframe.setSize(size);
+
+
 
     }
 
@@ -24,6 +53,9 @@ public class MainMenuView implements View {
     @Override
     public void start() {
 
+        this.setPreferredSize(size);
+        this.repaint();
+        jframe.setVisible(true);
     }
 
     /**
@@ -32,7 +64,15 @@ public class MainMenuView implements View {
     @Override
     public void render() {
 
+
+
     }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, null);
+    }
+
 
     /**
      * {@inheritDoc}
