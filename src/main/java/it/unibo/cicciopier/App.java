00fc06a097f3b79ac58a25c3a58546c8377e9af1
Md Shokot Alam <mdshokot.alam@studio.unibo.it>
2022-04-12 17:36:02 +0200

    Load all the animation in App

--- src/main/java/it/unibo/cicciopier/App.java
+++ src/main/java/it/unibo/cicciopier/App.java
@@ -2,21 +2,28 @@ package it.unibo.cicciopier;
 
 import it.unibo.cicciopier.controller.GameEngine;
 import it.unibo.cicciopier.model.Sound;
-import it.unibo.cicciopier.view.LoadAnimation;
+import it.unibo.cicciopier.view.Animation;
 import it.unibo.cicciopier.view.Texture;
 import it.unibo.cicciopier.model.Music;
+import it.unibo.cicciopier.view.entities.ExplosionView;
+import it.unibo.cicciopier.view.entities.LaserView;
+import it.unibo.cicciopier.view.entities.PlayerView;
+import it.unibo.cicciopier.view.entities.enemies.BroccoliView;
+import it.unibo.cicciopier.view.items.CoinView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
+import java.util.Collection;
 
 public final class App {
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
     public static void main(final String[] args) {
         LOGGER.info("Starting...");
+        //load all the textures
         for (Texture t : Texture.values()) {
             try {
                 t.load();
@@ -25,6 +32,13 @@ public final class App {
                 System.exit(1);
             }
         }
+        //load all the animation
+        loadAnimations(PlayerView.ANIMATIONS.values());
+        loadAnimations(BroccoliView.ANIMATIONS.values());
+        loadAnimation(CoinView.ANIMATION);
+        loadAnimation(ExplosionView.ANIMATION);
+        loadAnimation(LaserView.ANIMATION);
+
         //load all the sounds
         for (Sound sound : Sound.values()) {
             try {
@@ -41,15 +55,29 @@ public final class App {
                 e.printStackTrace();
             }
         }
-        //load all the animation for the game
-        LoadAnimation loadAnimation = LoadAnimation.getLoadAnimation();
-        loadAnimation.loadAllAnimation();
         try {
             GameEngine e = new GameEngine(null, "level-1-1.tmx");
             e.load();
             e.start();
         } catch (Exception e) {
             LOGGER.error("Error starting game...", e);
+            System.exit(1);
+        }
+    }
+
+    private static void loadAnimations(final Collection<Animation> animations) {
+        //load all the animations
+        for (Animation a : animations) {
+            loadAnimation(a);
+        }
+    }
+
+    private static void loadAnimation(final Animation a) {
+        try {
+            //LOGGER.info("Loading animation...");
+            a.load();
+        } catch (Exception e) {
+            LOGGER.error("Error loading animation...", e);
             System.exit(1);
         }
     }
